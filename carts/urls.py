e9a0356928daca5ce70608b9c76023efc4a0cd14
@tusharv1 <you@example.com>
2022-12-08 19:08:13 +0530

    add cart functionality

---
+++ carts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [ 
+   path("",views.cart,name="Cart"),
+   path("add_item/<int:product_id>/",views.add_cart,name="add_cart"),
+   path("remove_item/<int:product_id>/<int:cart_item_id>",views.remove_cart,name="remove_cart"),
+   path("remove_cart_item/<int:product_id>/<int:cart_item_id>",views.remove_cart_item,name="remove_cart_item")
+
+
+] 
--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [ 
     path("",views.store,name="Store"),  
-    path("<slug:category_slug>/",views.store,name="Product_by_category"),
-    path("<slug:category_slug>/<slug:product_slug>/",views.product_detail,name="Product_Detail")
+    path("category/<slug:category_slug>/",views.store,name="Product_by_category"),
+    path("category/<slug:category_slug>/<slug:product_slug>/",views.product_detail,name="Product_Detail"),
+    path("search/",views.search,name="Search"),
 ] 
