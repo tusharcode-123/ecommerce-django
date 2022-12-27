b9fff451a0d6a89bde09ab02cb06db97840af833
@tusharv1 <you@example.com>
2022-12-27 10:10:10 +0530

    add dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
 
+
+    path("my_order/",views.my_order,name="my_order"),
+    path("edit_profile/",views.edit_profile,name="edit_profile"),
+    path("change_password/",views.change_password,name="change_password"),
+    path("order_detail/<int:order_id>/",views.order_detail,name="order_detail")
+
     
 ] 
